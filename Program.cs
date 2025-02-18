a4b97200d789325ac811065b79b739edb9a6300b
Sanjoy Kumar Mahato <sanjoymahato.2356.csedu@gmail.com>
2025-02-18 13:34:16 +0600

    change in program file

--- Program.cs
+++ Program.cs
@@ -63,11 +63,8 @@ Log.Logger = new LoggerConfiguration()
     var app = builder.Build();
 
     // Configure the HTTP request pipeline.
-    if (app.Environment.IsDevelopment())
-    {
-        app.UseSwagger();
-        app.UseSwaggerUI();
-    }
+    app.UseSwagger();
+    app.UseSwaggerUI();
 
     app.UseHttpsRedirection();
     app.UseAuthentication();
